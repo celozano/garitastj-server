494871ac6a8813c1ffb084e86269d1576b8f1b98
celozano <enlozanor@gmail.com>
2023-05-20 12:14:34 -0700

    fix redis config

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,18 @@ import { parse } from './utils';
 
 const PORT = process.env.PORT || 3000;
 const BWT_URL = process.env.BWT_URL;
-const REDIS_URL = process.env.REDIS_URL;
+const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_USERNAME = process.env.REDIS_USERNAME;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
 const app: Express = express();
-const redis = new Redis(REDIS_URL);
+const redis = new Redis({
+  port: 6379,
+  host: REDIS_HOST,
+  username: REDIS_USERNAME,
+  password: REDIS_PASSWORD,
+  family: 6,
+});
 
 app.use(cors({ origin: '*' }));
 
