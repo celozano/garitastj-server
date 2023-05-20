87811ae3a50e95e6e838ef379071f549c7e16913
celozano <enlozanor@gmail.com>
2023-05-20 11:15:43 -0700

    caching api responses with redis

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,33 @@ require('dotenv').config();
 import axios from 'axios';
 import cors from 'cors';
 import express, { Express, Response } from 'express';
+import Redis from 'ioredis';
 
 import { parse } from './utils';
 
 const PORT = process.env.PORT || 3000;
 const BWT_URL = process.env.BWT_URL;
+const REDIS_URL = process.env.REDIS_URL;
 
 const app: Express = express();
+const redis = new Redis(REDIS_URL);
 
 app.use(cors({ origin: '*' }));
 
 app.get('/border-wait-times', async (_, res: Response) => {
   try {
-    const response = await axios.get(BWT_URL);
-    const json = await parse(response.data);
+    const cache = await redis.get('border-wait-times');
+    if (cache) {
+      res.send({ ...JSON.parse(cache), source: 'redis' });
+      return;
+    }
 
-    res.send(json);
+    const response = await axios.get(BWT_URL);
+    const data = await parse(response.data);
+
+    redis.set('border-wait-times', JSON.stringify(data), 'EX', 1800);
+
+    res.send(data);
   } catch (error) {
     res.status(500).send('Something went wrong.');
   }
